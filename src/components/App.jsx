f6046daf5d172df042b99a59c002ecc607094beb
Adam <garamone@gmail.com>
2021-01-31 23:47:59 +0100

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Body = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: black;
+  color: white;
+  text-align: center;
+`;
+
+function App() {
+  return <Body>Test</Body>;
+}
+
+export default App;
